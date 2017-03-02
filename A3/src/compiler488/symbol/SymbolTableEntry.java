98358f31e5fe36c52b1041286f8df6b1733cf84c
theopoenaru <theo.poenaru@gmail.com>
2017-03-02 17:25:26 -0500

    symtable stuff

---
+++ A3/src/compiler488/symbol/SymbolTableEntry.java
@@ -0,0 +1,63 @@
+package compiler488.symbol;
+
+import compiler488.ast.type.Type;
+import compiler488.ast.AST;
+
+/**
+ * Created by theo on 2017-03-02.
+ */
+public class SymbolTableEntry {
+
+    /**
+     *  Identifier for this symbol
+     */
+    private String name;
+
+    private Type type;
+
+    /* level of scope*/
+    private int level;
+
+    /* Value of variable */
+    private AST value;
+
+    /* position within this scopes*/
+    private int orderNo;
+
+    public SymbolTableEntry(String name, AST value, Type type) {
+        this.name  = name;
+        this.value = value;
+        this.type  = type;
+    }
+
+    /* Modifiers ID */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public SymbolTableEntry setIdentifier(String identifier) {
+        this.identifier = identifier;
+        return this;
+    }
+
+    /* Modifiers Value */
+    public AST getValue() {
+        return value;
+    }
+
+    public SymbolTableEntry setValue(AST value) {
+        this.value = value;
+        return this;
+    }
+
+    /* Modifiers Type */
+    public Type getType() {
+        return type;
+    }
+
+    public SymbolTableEntry setType(Type type) {
+        this.type = type;
+        return this;
+    }
+
+}
